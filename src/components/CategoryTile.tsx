6d5ee8840b370787b96b26e086c23c22fa08d546
damonfdo <damonfdo@live.com>
2024-06-19 23:01:45 +0530

    Fix Category Tile Structure

--- src/components/CategoryTile.tsx
+++ src/components/CategoryTile.tsx
@@ -7,13 +7,12 @@ const CategoryTile: React.FC<Category> = ({ name, slug }) => {
   const bgColor = getBgColorByCategory(slug);
 
   return (
-    <Link to={`/category/${slug}`}>
-      <div
-        className="flex max-w-sm h-28 mx-auto  shadow-lg rounded-[1rem] border cursor-pointer items-center justify-center px-4"
-        style={{ backgroundColor: bgColor }}
-      >
-        <p className="text-gray-200  text-2xl font-bold">{name}</p>
-      </div>
+    <Link
+      to={`/category/${slug}`}
+      className="flex w-[100%] h-32 mx-auto  shadow-lg rounded-[1rem] border cursor-pointer items-center justify-center px-4"
+      style={{ backgroundColor: bgColor }}
+    >
+      <p className="text-gray-200  text-2xl font-bold">{name}</p>
     </Link>
   );
 };
