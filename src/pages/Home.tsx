1da4044aa87bd1961bc101f65d24ebc8899d203b
damonfdo <damonfdo@live.com>
2024-06-19 23:02:10 +0530

    Fix Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,18 +16,22 @@ const Home: React.FC = () => {
     return <Loading />;
   }
   return (
-    <div className="container flex flex-col gap-8">
-      <section id="flashSale" className="flex flex-col gap-8">
+    <div className="container flex flex-col gap-8 my-12">
+      <section id="flashSale" className="flex flex-col gap-8 ">
         <SectionTitle name="Flash Sale" />
-        <div className="flex flex-row flex-wrap">
-          {products.map((product) => (
-            <ProductCard key={product.id} {...product} />
-          ))}
+        <div className="overflow-x-auto">
+          <div className="flex flex-row flex-nowrap space-x-4">
+            {products.map((product) => (
+              <div className="w-1/4 min-w-[25%]" key={product.id}>
+                <ProductCard {...product} />
+              </div>
+            ))}
+          </div>
         </div>
       </section>
       <section id="categories" className="flex flex-col gap-8">
         <SectionTitle name="Categories" />
-        <div className="flex w-[100%] flex-row h-[40rem] gap-4 overflow-x-hidden">
+        <div className="flex flex-row justify-between gap-4 w-[100%]">
           {CATEGORIES.map((category) => (
             <CategoryTile key={category.slug} {...category} />
           ))}
