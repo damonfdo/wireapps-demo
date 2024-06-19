2196a198f52407dab134b050f4eb0924b4aa17c6
damonfdo <damonfdo@live.com>
2024-06-20 00:13:15 +0530

    Update skeleton loader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const Loading: React.FC = () => {
   return (
-    <div role="status" className="max-w-sm animate-pulse">
-      <div className="h-4 bg-gray-200 rounded-full w-48 mb-4" />
-      <div className="h-4 bg-gray-200 rounded-full max-w-[360px] mb-2.5" />
-      <div className="h-4 bg-gray-200 rounded-full mb-2.5" />
-      <div className="h-2 bg-gray-200 rounded-full max-w-[330px] mb-2.5" />
-      <div className="h-2 bg-gray-200 rounded-full max-w-[300px] mb-2.5" />
-      <div className="h-2 bg-gray-200 rounded-full max-w-[360px]" />
-      <span className="sr-only">Loading...</span>
+    <div
+      role="status"
+      className="flex flex-row flex-nowrap space-x-4 animate-pulse "
+    >
+      {[1, 2, 3, 4].map((data) => (
+        <div className="w-1/4 min-w-[25%]" key={data}>
+          <div className="h-[30rem] bg-gray-200 rounded-lg max-w-sm mb-4" />
+        </div>
+      ))}
     </div>
   );
 };
